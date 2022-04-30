dd395ef44e77ededd20f842752e7d93d234443a2
Gautam <gautamsharma2813@gmail.com>
2022-04-30 13:39:03 -0400

    fixed links

--- src/postsInfo.js
+++ src/postsInfo.js
@@ -4,7 +4,7 @@ export var Array = [
         "title": "Using auto",
         "summary": "With C++11 came a new keyword called auto. This has made writing and reading programs so much simpler.",
         "fileLocation": "null",
-        "link":"null",
+        "link":"/posts/1",
         "img": {
             "path": "../resources/post1.jpg",
             "alt": "Using auto"
@@ -15,7 +15,7 @@ export var Array = [
         "title": "hello world",
         "summary": "i dont knownull",
         "fileLocation": "null",
-        "link":"null",
+        "link":"/posts/2",
         "img": {
             "path": "null",
             "alt": "null"
